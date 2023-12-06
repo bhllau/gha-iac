f4c7ce600ef750e81422282487fcfb828923876b
Bruce <hllau@hllau.com>
2023-12-07 10:19:35 +1100

    Add TF modules restoration

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,18 @@ setup() {
 		echo "--> Work dir "$WORKDIR" does not exist. Exiting..." >&2
 		exit 0
 	fi
+	cd "$WORKDIR"
 
 	create_gitconfig
-	cd "$WORKDIR"
 	if echo "$COMMAND" | grep -q '^terraform '; then
-		terraform init
+		restore_tf_modules \
+			&& terraform init
 	fi
 }
 
 teardown() {
-	rm -f ~/.gitconfig
+	save_tf_modules \
+		&& rm -f ~/.gitconfig
 }
 
 extract_args() {
@@ -24,6 +26,7 @@ extract_args() {
 	local aws_secret_access_key=$1; shift
 	local aws_region=$1; shift
 	local tf_cli_args_init=$1; shift
+	local tf_modules_restore_path=$1; shift
 	local command=$1; shift
 
 	export WORKDIR=$workdir
@@ -33,6 +36,7 @@ extract_args() {
 	export AWS_REGION=$aws_region
 	export AWS_DEFAULT_REGION=$aws_region
 	export TF_CLI_ARGS_init=$tf_cli_args_init
+	export TF_MODULES_RESTORE_PATH=$tf_modules_restore_path
 	export COMMAND=$command
 }
 
@@ -41,6 +45,30 @@ create_gitconfig() {
 [url "https://oauth2:${GITHUB_TOKEN}@github.com"]
 	insteadOf = https://github.com
 EOF
+}
+
+restore_tf_modules() {
+	local filename=.terraform.tar.gz
+	local target="$TF_MODULES_RESTORE_PATH"/$filename
+	if [ ! -d .terraform ] \
+		&& [ -n "$TF_MODULES_RESTORE_PATH" ] \
+		&& aws s3 ls "$target" --quiet; then
+		echo '--> Restoring terraform modules...' >&2
+		aws s3 cp "$target" ./"$filename" --quiet \
+			&& tar -xzf "$filename" \
+			&& rm -f "$filename"
+	fi
+}
+
+save_tf_modules() {
+	local filename=.terraform.tar.gz
+	local target="$TF_MODULES_RESTORE_PATH"/$filename
+	if [ -d .terraform ] && [ -n "$TF_MODULES_RESTORE_PATH" ]; then
+		echo '--> Saving terraform modules...' >&2
+		tar -czf "$filename" .terraform .terraform.lock.hcl \
+			&& aws s3 cp "$filename" "$target" --quiet \
+			&& rm -f "$filename"
+	fi
 }
 
 parse_and_run_command() {
